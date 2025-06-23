40552dc923ae0ff52dead000ed560a1b05bc3588
curlyheadcoder <mayanksonwani078@gmail.com>
2025-06-24 01:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+function addListItems(count) {
+  for (let i = 0; i < count; i++) {
+    const listItem = document.createElement("li");
+    console.log("length of children---", `${list.children.length}`);
+    listItem.textContent = `Item ${list.children.length + 1}`;
+    list.appendChild(listItem);
+  }
+}
+
+addListItems(10);
+
+list.addEventListener("scroll", function () {
+  if (this.scrollTop + this.clientHeight >= this.scrollHeight) {
+    addListItems(2);
+  }
+});
